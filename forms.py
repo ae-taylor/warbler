c5cf9a60a49683e0f878b8e0f4fb3b5f39256676
Andre Pautin <andre.pautin@gmail.com>
2021-05-05 12:17:56 -0700

    add form to update user

--- forms.py
+++ forms.py
@@ -18,8 +18,19 @@ class UserAddForm(FlaskForm):
     image_url = StringField('(Optional) Image URL')
 
 
+class UserUpdateForm(FlaskForm):
+    """Form for updating user """
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Image URL')
+    bio = StringField('Bio')
+    password = PasswordField('Password', validators=[Length(min=6)])
+
 class LoginForm(FlaskForm):
     """Login form."""
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
